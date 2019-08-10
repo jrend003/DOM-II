533d0ec57e767179daa05fb6cd84183c1d1240ae
marta rendon <jrend003@gmail.com>
2019-08-09 21:37:21 -0400

    added event listeners and stop propagation to nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const nav = document.getElementsByClassName('nav');
+nav.addEventListener("mouseover", (event) =>{ event.target.style.color = "orange"});
+
+const button=document.getElementsByClassName('btn');
+button.addEventListener("click", (event) =>{console.log(button)});
+
+const el = document.querySelector('div');
+el.addEventListener('wheel', zoom);
+
+const navDoubleClick= document.GetElementsByClassName('nav');
+navDoubleClick.addEventListener ("dblclick, event") =>{console.log(navDoubleClick)};
+
+const intro = document.getElementsByClassName('intro');
+intro.addEventListener('focus', (event) => {
+event.target.style.background = 'pink';    
+});
+
+const textContent = document.getElementsByClassName(text-content);
+textContent.addEventListener('blur', (event) => {
+ event.target.style.background = 'orange';    
+});
+
+const navItems = document.getElementsByClassName('nav');
+(event) => { event.stopPropagation() };
